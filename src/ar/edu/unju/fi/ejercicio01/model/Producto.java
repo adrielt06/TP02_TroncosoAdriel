cf507fc2540ccbb66ce4e4138b883957214f74b2
ADRIEL <ADRIEL@DESKTOP-OP9TCVA>
2024-04-25 17:42:37 -0300

    Iniciando ejercicio 1 - Troncoso Adriel

--- src/ar/edu/unju/fi/ejercicio01/model/Producto.java
+++ src/ar/edu/unju/fi/ejercicio01/model/Producto.java
@@ -1,5 +1,59 @@
 package ar.edu.unju.fi.ejercicio01.model;
 
 public class Producto {
+	private String codigo;
+	private String descripcion;
+	private double precioU;
+	private OrigenFabricacion origenFabricacion;
+	private Categoria categoria;
+	
+	
+	
+	public String getCodigo() {
+		return codigo;
+	}
 
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecioU() {
+		return precioU;
+	}
+
+	public void setPrecioU(double precioU) {
+		this.precioU = precioU;
+	}
+
+	public OrigenFabricacion getOrigenFabricacion() {
+		return origenFabricacion;
+	}
+
+	public void setOrigenFabricacion(OrigenFabricacion origenFabricacion) {
+		this.origenFabricacion = origenFabricacion;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public enum OrigenFabricacion{
+		ARGENTINA, CHINA, BRASIL, URUGUAY
+	}
+	
+	public enum Categoria{
+		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
+	}
 }
