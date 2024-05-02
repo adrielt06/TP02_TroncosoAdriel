62d1cc79aa37f821c00644d51f192cdb5f7d17fc
ADRIEL <ADRIEL@DESKTOP-OP9TCVA>
2024-05-01 21:51:21 -0300

    Ejercicio 4 - Troncoso Adriel

---
+++ src/ar/edu/unju/fi/ejercicio04/util/Listado.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio04.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.fi.ejercicio04.model.Jugador;
+
+public class Listado {
+	public static List<Jugador> jugadores = new ArrayList<Jugador>();
+}
